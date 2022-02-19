efa4da99bd75e2515455458683cab742fb9d950c
Julien Hache <julien.hache7@gmail.com>
2022-02-19 19:13:21 +0100

    Fix full_process script for data deployment.

--- scripts/full_process.ts
+++ scripts/full_process.ts
@@ -8,11 +8,18 @@ import { ethers } from "hardhat";
 import { generateMetadata } from "../ipfs/generate_metadata";
 import { pinToPinata } from "../ipfs/pin_to_pinata";
 import path from "path";
+import fs from "fs";
 import Ctl from "ipfsd-ctl";
 import { assert } from "console";
-
+import { ImportCandidate} from "ipfs-core-types/types/src/utils";
+import { AddResult } from "ipfs-core-types/src/root";
 
 const deployData = async () => {
+  const ipfsImagesDir = "images";
+  const localImagesDir = path.resolve("data/", ipfsImagesDir);
+  const ipfsMetadataDir = "metadata";
+  const localMetadataDir = path.resolve("data/", ipfsMetadataDir);
+
   // 1: spawn an IFPS node
   const ipfsd = await Ctl.createController({
     ipfsHttpModule: require('ipfs-http-client'),
@@ -21,27 +28,43 @@ const deployData = async () => {
     disposable: true,
   });
 
-  // todo: ipfs.api.add doesn't read the directory content
   try {
     console.log("starting data deployment...");
+
     // 2: deploy images to pinata.cloud
-    const images = await ipfsd.api.add({ path: path.resolve("data/images") });
-    const imagesPin = await pinToPinata(images.cid.toString(), "images");
-    // assert(imagesPin.status !== "failed");
-    console.log("images deployed.");
+    const imagesFiles: string[] = fs.readdirSync(localImagesDir);
+    const imagesToImport: ImportCandidate[] = [];
+    imagesToImport.push(...(imagesFiles.map<ImportCandidate>(
+      (file) => ({
+        path: ipfsImagesDir + "/" + file,
+        content: fs.readFileSync(path.resolve(localImagesDir, file))
+      }))));
+    const imagesPromise = await ipfsd.api.addAll(imagesToImport);
+    let images: AddResult;
+    for await (images of imagesPromise);//get last element: the directory
+
+    const imagesPin = await pinToPinata(images!.cid.toString(), "images");
+    console.log("images deployed. CID= " + imagesPin.cid.toString());
 
     // 3: generate metadata entries
     const metadataDir = generateMetadata(imagesPin.cid.toString(), path.resolve("data"));
 
     // 4: deploy metadata to pinata.cloud
-    const metadata = await ipfsd.api.add({ path: path.resolve(metadataDir) });
-    const metadataPin = await pinToPinata(metadata.cid.toString(), "metadata");
-    // assert(metadataPin.status !== "failed");
-    console.log("metadata deployed.");
+    const metadataFiles: string[] = fs.readdirSync(localMetadataDir);
+    const metadataToImport: ImportCandidate[] = [];
+    metadataToImport.push(...(metadataFiles.map<ImportCandidate>(
+      (file) => ({
+        path: ipfsMetadataDir + "/" + file,
+        content: fs.readFileSync(path.resolve(localMetadataDir, file))
+      }))));
+    const metadataPromise = await ipfsd.api.addAll(metadataToImport);
+    let metadata: AddResult;
+    for await (metadata of metadataPromise);//get last element: the directory
+    
+    const metadataPin = await pinToPinata(metadata!.cid.toString(), "metadata");
+    console.log("metadata deployed. CID= " + metadataPin.cid.toString());
 
     console.log("\nThe deployment succeed.");
-    console.log("Images CID: " + imagesPin.cid.toString());
-    console.log("Metadata CID: " + metadataPin.cid.toString());
   }
   catch (e: any) {
     console.log(e);
